53e029dbd509a4ad52eff04600ff2149a555d73c
shubham maurya <131405109+shubhammauryainfo@users.noreply.github.com>
2025-01-24 16:33:23 +0530

    upsate

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "auth-key"],
   })
